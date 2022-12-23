f3a656ba2a5ef5aa3ebf2602d1402a22db260a53
Cesar Fajardo <cesarjavierfajardoortiz@gmail.com>
2022-12-22 20:43:00 -0500

    Solución final ejercicio 1

--- python/ejercicio_1.py
+++ python/ejercicio_1.py
@@ -2,4 +2,5 @@
 # Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
 # Tienes que subir capturas de pantalla en una carpeta comprimida zip.
 
-print("Hola mundo!")
+mensaje = "Hola mundo!"
+print(mensaje)
